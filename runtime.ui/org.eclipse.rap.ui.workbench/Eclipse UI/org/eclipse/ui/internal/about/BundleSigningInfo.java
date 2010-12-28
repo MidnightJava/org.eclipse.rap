d01efdeb51ca435cc98a5f83c711debec2d9ddb9
rsternber <rsternber>
2010-12-28 14:47:58 +0000

    Fixed bug 309661: Update workbench code to Indigo (3.7) https://bugs.eclipse.org/bugs/show_bug.cgi?id=309661

--- runtime.ui/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/BundleSigningInfo.java
+++ runtime.ui/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/ui/internal/about/BundleSigningInfo.java
@@ -1,6 +1,6 @@
-// RAP [rh] AboutDialog left out for now
+// RAP [bm] AboutDialog left out for now
 ///*******************************************************************************
-// * Copyright (c) 2007, 2008 IBM Corporation and others.
+// * Copyright (c) 2007, 2010 IBM Corporation and others.
 // * All rights reserved. This program and the accompanying materials
 // * are made available under the terms of the Eclipse Public License v1.0
 // * which accompanies this distribution, and is available at
@@ -10,7 +10,7 @@
 // *     IBM Corporation - initial API and implementation
 // *******************************************************************************/
 //
-//package org.eclipse.ui.internal.dialogs;
+//package org.eclipse.ui.internal.about;
 //
 //import java.io.IOException;
 //import java.security.GeneralSecurityException;
@@ -22,18 +22,15 @@
 //import java.util.Iterator;
 //import java.util.List;
 //import java.util.Map;
+//import java.util.Map.Entry;
 //import java.util.Properties;
 //import java.util.StringTokenizer;
-//import java.util.Map.Entry;
-//
 //import org.eclipse.core.runtime.IProgressMonitor;
 //import org.eclipse.core.runtime.IStatus;
 //import org.eclipse.core.runtime.OperationCanceledException;
 //import org.eclipse.core.runtime.Status;
 //import org.eclipse.core.runtime.jobs.Job;
 //import org.eclipse.jface.dialogs.Dialog;
-//import org.eclipse.jface.dialogs.DialogTray;
-//import org.eclipse.jface.dialogs.TrayDialog;
 //import org.eclipse.jface.resource.JFaceResources;
 //import org.eclipse.osgi.signedcontent.SignedContent;
 //import org.eclipse.osgi.signedcontent.SignedContentFactory;
@@ -41,156 +38,152 @@
 //import org.eclipse.osgi.util.NLS;
 //import org.eclipse.swt.SWT;
 //import org.eclipse.swt.custom.StyledText;
-//import org.eclipse.swt.graphics.Color;
 //import org.eclipse.swt.graphics.GC;
 //import org.eclipse.swt.graphics.Point;
 //import org.eclipse.swt.layout.GridData;
 //import org.eclipse.swt.layout.GridLayout;
 //import org.eclipse.swt.widgets.Composite;
 //import org.eclipse.swt.widgets.Control;
-//import org.eclipse.swt.widgets.Display;
 //import org.eclipse.swt.widgets.Label;
-//import org.eclipse.swt.widgets.Shell;
 //import org.eclipse.swt.widgets.Text;
+//import org.eclipse.ui.PlatformUI;
 //import org.eclipse.ui.internal.WorkbenchMessages;
 //import org.eclipse.ui.internal.WorkbenchPlugin;
-//import org.eclipse.ui.internal.about.AboutBundleData;
 //import org.eclipse.ui.statushandlers.StatusManager;
 //import org.osgi.framework.BundleContext;
 //import org.osgi.framework.ServiceReference;
 //
 ///**
 // * @since 3.3
-// *
+// * 
 // */
-//public class BundleSigningTray extends DialogTray {
+//public class BundleSigningInfo {
 //
-//
+//	private Composite composite;
 //	private Text date;
 //	private StyledText certificate;
 //	private AboutBundleData data;
-//	private TrayDialog dialog;
-//	
-//	/**
-//	 * 
-//	 */
-//	public BundleSigningTray(TrayDialog dialog) {
-//		this.dialog = dialog;
+//
+//	public BundleSigningInfo() {
 //	}
-//	
+//
 //	public void setData(AboutBundleData data) {
 //		this.data = data;
 //		startJobs();
 //	}
 //
-//	/* (non-Javadoc)
-//	 * @see org.eclipse.jface.dialogs.DialogTray#createContents(org.eclipse.swt.widgets.Composite)
-//	 */
-//	protected Control createContents(Composite parent) {
-//		Composite content = new Composite(parent, SWT.NONE);
-//		content.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+//	public Control createContents(Composite parent) {
+//
+//		composite = new Composite(parent, SWT.BORDER);
+//		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 //		GridLayout layout = new GridLayout(2, false);
-//		content.setLayout(layout);
+//		layout.marginHeight = 0;
+//		layout.marginWidth = 0;
+//		composite.setLayout(layout);
+//
 //		// date
-//		Color backgroundColor = parent.getDisplay().getSystemColor(
-//				SWT.COLOR_WIDGET_BACKGROUND);
 //		{
-//			Label label = new Label(content, SWT.NONE);
-//			label.setText(WorkbenchMessages.BundleSigningTray_Signing_Date); 
+//			Label label = new Label(composite, SWT.NONE);
+//			label.setText(WorkbenchMessages.BundleSigningTray_Signing_Date);
 //			GridData data = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
-//			date = new Text(content, SWT.READ_ONLY);
+//			date = new Text(composite, SWT.READ_ONLY);
 //			GC gc = new GC(date);
 //			gc.setFont(JFaceResources.getDialogFont());
-//			Point size = gc.stringExtent(DateFormat.getDateTimeInstance().format(new Date()));
+//			Point size = gc.stringExtent(DateFormat.getDateTimeInstance()
+//					.format(new Date()));
 //			data.widthHint = size.x;
 //			gc.dispose();
-//			date.setText(WorkbenchMessages.BundleSigningTray_Working); 
+//			date.setText(WorkbenchMessages.BundleSigningTray_Working);
 //			date.setLayoutData(data);
-//			date.setBackground(backgroundColor);
 //		}
 //		// signer
 //		{
-//			Label label = new Label(content, SWT.NONE);
-//			label.setText(WorkbenchMessages.BundleSigningTray_Signing_Certificate); 
-//			GridData data = new GridData(SWT.BEGINNING, SWT.BEGINNING, true, false);
+//			Label label = new Label(composite, SWT.NONE);
+//			label
+//					.setText(WorkbenchMessages.BundleSigningTray_Signing_Certificate);
+//			GridData data = new GridData(SWT.BEGINNING, SWT.BEGINNING, true,
+//					false);
 //			data.horizontalSpan = 2;
 //			data = new GridData(SWT.FILL, SWT.FILL, true, true);
 //			data.horizontalSpan = 2;
-//			certificate = new StyledText(content, SWT.READ_ONLY | SWT.MULTI | SWT.WRAP);
-//			certificate.setText(WorkbenchMessages.BundleSigningTray_Working); 
+//			certificate = new StyledText(composite, SWT.READ_ONLY | SWT.MULTI
+//					| SWT.WRAP);
+//			certificate.setText(WorkbenchMessages.BundleSigningTray_Working);
 //			certificate.setLayoutData(data);
 //		}
-//		
-//		// problems
-////		{
-////			Label label = new Label(content, SWT.NONE);
-////			label.setText("Problems:"); //$NON-NLS-1$
-////
-////		}
-//		Dialog.applyDialogFont(content);
+//		Dialog.applyDialogFont(composite);
 //
 //		startJobs(); // start the jobs that will prime the content
-//
-//		return content;
+//		return composite;
 //	}
-//	
+//
 //	/**
 //	 * 
 //	 */
 //	private void startJobs() {
 //		if (!isOpen())
 //			return;
-//		certificate.setText(WorkbenchMessages.BundleSigningTray_Working); 
-//		date.setText(WorkbenchMessages.BundleSigningTray_Working); 
+//		certificate.setText(WorkbenchMessages.BundleSigningTray_Working);
+//		date.setText(WorkbenchMessages.BundleSigningTray_Working);
 //		final BundleContext bundleContext = WorkbenchPlugin.getDefault()
 //				.getBundleContext();
 //		final ServiceReference factoryRef = bundleContext
 //				.getServiceReference(SignedContentFactory.class.getName());
 //		if (factoryRef == null) {
-//			StatusManager.getManager().handle(
-//					new Status(IStatus.WARNING, WorkbenchPlugin.PI_WORKBENCH,
-//							WorkbenchMessages.BundleSigningTray_Cant_Find_Service), 
-//					StatusManager.LOG);
+//			StatusManager
+//					.getManager()
+//					.handle(
+//							new Status(
+//									IStatus.WARNING,
+//									WorkbenchPlugin.PI_WORKBENCH,
+//									WorkbenchMessages.BundleSigningTray_Cant_Find_Service),
+//							StatusManager.LOG);
 //			return;
 //		}
 //
 //		final SignedContentFactory contentFactory = (SignedContentFactory) bundleContext
 //				.getService(factoryRef);
 //		if (contentFactory == null) {
-//			StatusManager.getManager().handle(
-//					new Status(IStatus.WARNING, WorkbenchPlugin.PI_WORKBENCH,
-//							WorkbenchMessages.BundleSigningTray_Cant_Find_Service), 
-//					StatusManager.LOG);
+//			StatusManager
+//					.getManager()
+//					.handle(
+//							new Status(
+//									IStatus.WARNING,
+//									WorkbenchPlugin.PI_WORKBENCH,
+//									WorkbenchMessages.BundleSigningTray_Cant_Find_Service),
+//							StatusManager.LOG);
 //			return;
 //		}
 //
 //		final AboutBundleData myData = data;
-//		final Job signerJob = new Job(NLS.bind(WorkbenchMessages.BundleSigningTray_Determine_Signer_For, myData.getId())) { 
+//		final Job signerJob = new Job(NLS.bind(
+//				WorkbenchMessages.BundleSigningTray_Determine_Signer_For,
+//				myData.getId())) {
 //
 //			protected IStatus run(IProgressMonitor monitor) {
+//				final String[] signingInfo = new String[] {
+//						WorkbenchMessages.BundleSigningTray_Unsigned,
+//						WorkbenchMessages.BundleSigningTray_Unsigned };
 //				try {
 //					if (myData != data)
 //						return Status.OK_STATUS;
-//					SignedContent signedContent = contentFactory.getSignedContent(myData
-//							.getBundle());
+//					SignedContent signedContent = contentFactory
+//							.getSignedContent(myData.getBundle());
 //					if (myData != data)
 //						return Status.OK_STATUS;
 //					SignerInfo[] signers = signedContent.getSignerInfos();
-//					final String signerText, dateText;
-//					final Shell dialogShell = dialog.getShell();
-//					if (!isOpen() && BundleSigningTray.this.data == myData)
+//					if (!isOpen() && BundleSigningInfo.this.data == myData)
 //						return Status.OK_STATUS;
 //
-//					if (signers.length == 0) {
-//						signerText = WorkbenchMessages.BundleSigningTray_Unsigned; 
-//						dateText = WorkbenchMessages.BundleSigningTray_Unsigned; 
-//					} else {
-//						Properties [] certs = parseCerts(signers[0].getCertificateChain());
+//					if (signers.length > 0) {
+//						Properties[] certs = parseCerts(signers[0]
+//								.getCertificateChain());
 //						if (certs.length == 0)
-//							signerText = WorkbenchMessages.BundleSigningTray_Unknown; 
+//							signingInfo[0] = WorkbenchMessages.BundleSigningTray_Unknown;
 //						else {
 //							StringBuffer buffer = new StringBuffer();
-//							for (Iterator i = certs[0].entrySet().iterator(); i.hasNext(); ) {
+//							for (Iterator i = certs[0].entrySet().iterator(); i
+//									.hasNext();) {
 //								Map.Entry entry = (Entry) i.next();
 //								buffer.append(entry.getKey());
 //								buffer.append('=');
@@ -198,36 +191,40 @@
 //								if (i.hasNext())
 //									buffer.append('\n');
 //							}
-//							signerText = buffer.toString();
+//							signingInfo[0] = buffer.toString();
 //						}
 //
-//						Date signDate = signedContent.getSigningTime(signers[0]);
+//						Date signDate = signedContent
+//								.getSigningTime(signers[0]);
 //						if (signDate != null)
-//							dateText = DateFormat.getDateTimeInstance().format(
+//							signingInfo[1] = DateFormat.getDateTimeInstance().format(
 //									signDate);
 //						else
-//							dateText = WorkbenchMessages.BundleSigningTray_Unknown; 
+//							signingInfo[1] = WorkbenchMessages.BundleSigningTray_Unknown;
 //					}
-//					
-//					Display display = dialogShell.getDisplay();
-//					display.asyncExec(new Runnable() {
-//
-//						public void run() {
-//							// check to see if the tray is still visible and if we're still looking at the same item
-//							if (!isOpen() && BundleSigningTray.this.data != myData)
-//								return;
-//							certificate.setText(signerText);
-//							date.setText(dateText);
-//						}
-//					});
-//
 //				} catch (IOException e) {
-//					return new Status(IStatus.ERROR,
-//							WorkbenchPlugin.PI_WORKBENCH, e.getMessage(), e);
+//					// default "unsigned info" is ok for the user, but log the
+//					// problem.
+//					StatusManager.getManager().handle(
+//							new Status(IStatus.ERROR, WorkbenchPlugin.PI_WORKBENCH, e.getMessage(),
+//									e), StatusManager.LOG);
 //				} catch (GeneralSecurityException e) {
-//					return new Status(IStatus.ERROR,
-//							WorkbenchPlugin.PI_WORKBENCH, e.getMessage(), e);
+//					// default "unsigned info is ok, but log the problem.
+//					StatusManager.getManager().handle(
+//							new Status(IStatus.ERROR, WorkbenchPlugin.PI_WORKBENCH, e.getMessage(),
+//									e), StatusManager.LOG);
 //				}
+//				PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+//					public void run() {
+//						// check to see if the tray is still visible
+//						// and if we're still looking at the same item
+//						if (!isOpen() && BundleSigningInfo.this.data != myData)
+//							return;
+//						certificate.setText(signingInfo[0]);
+//						date.setText(signingInfo[1]);
+//					}
+//				});
+//
 //				return Status.OK_STATUS;
 //			}
 //		};
@@ -235,7 +232,8 @@
 //		signerJob.belongsTo(signerJob);
 //		signerJob.schedule();
 //
-//		Job cleanup = new Job(WorkbenchMessages.BundleSigningTray_Unget_Signing_Service) { 
+//		Job cleanup = new Job(
+//				WorkbenchMessages.BundleSigningTray_Unget_Signing_Service) {
 //
 //			protected IStatus run(IProgressMonitor monitor) {
 //				try {
@@ -264,12 +262,12 @@
 //		for (int i = 0; i < chain.length; i++) {
 //			if (!(chain[i] instanceof X509Certificate))
 //				continue;
-//			Map cert = parseCert(((X509Certificate) chain[i]).getSubjectDN().getName());
+//			Map cert = parseCert(((X509Certificate) chain[i]).getSubjectDN()
+//					.getName());
 //			if (cert != null)
 //				certs.add(cert);
 //		}
-//		return (Properties []) certs.toArray(new Properties[certs.size()]);
-//
+//		return (Properties[]) certs.toArray(new Properties[certs.size()]);
 //
 //	}
 //
@@ -299,4 +297,7 @@
 //		return cert;
 //	}
 //
+//	public void dispose() {
+//		composite.dispose();
+//	}
 //}
--- runtime.ui/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/CommandMessages.java
+++ runtime.ui/org.eclipse.rap.ui.workbench/Eclipse UI/org/eclipse/ui/internal/menus/CommandMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,13 +9,23 @@
  *     IBM Corporation - initial API and implementation
  ******************************************************************************/
 
-package org.eclipse.ui.internal.services;
+package org.eclipse.ui.internal.menus;
 
-import org.eclipse.ui.services.IEvaluationService;
+import org.eclipse.osgi.util.NLS;
 
 /**
- *
+ * 
+ * @since 3.5
+ * 
  */
-public interface IRestrictionService extends IEvaluationService {
+public class CommandMessages extends NLS {
 
+	private static final String BUNDLE_NAME = "org.eclipse.ui.internal.menus.messages";//$NON-NLS-1$
+
+	static {
+		// load message values from bundle file
+		NLS.initializeMessages(BUNDLE_NAME, CommandMessages.class);
+	}
+
+	public static String Tooltip_Accelerator;
 }
