d3ec51cf93dfb8f0239d85d115782a179db55adf
rherrmann <rherrmann>
2011-06-11 13:49:33 +0000

    Changes to resolve bug 348587: RWT does not compile against javax.servlet 3.0

---
+++ runtime.rwt.test/org.eclipse.rap.rwt.cluster.testfixture.test/src/org/eclipse/rap/rwt/cluster/testfixture/internal/tomcat/TomcatEngine_Test.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.cluster.testfixture.internal.tomcat;
+
+import org.eclipse.rap.rwt.cluster.testfixture.internal.server.ServletEngine_Test;
+import org.eclipse.rap.rwt.cluster.testfixture.server.IServletEngineFactory;
+import org.eclipse.rap.rwt.cluster.testfixture.server.TomcatFactory;
+
+
+public class TomcatEngine_Test extends ServletEngine_Test {
+
+  protected IServletEngineFactory getServletEngineFactory() {
+    return new TomcatFactory();
+  }
+}
