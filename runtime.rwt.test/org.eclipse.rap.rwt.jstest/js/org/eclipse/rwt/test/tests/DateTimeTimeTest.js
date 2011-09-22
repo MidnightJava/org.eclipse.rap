f77126f1fc9dfbffe151df46c1a5d1e4826c750d
ifurnadjiev <ifurnadjiev>
2011-09-22 12:31:03 +0000

    Fixed - bug 358531: [DateTime] Editing with keyboard leads to wrong date under some constellations https://bugs.eclipse.org/bugs/show_bug.cgi?id=358531

---
+++ runtime.rwt.test/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/DateTimeTimeTest.js
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright (c) 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+qx.Class.define( "org.eclipse.rwt.test.tests.DateTimeTimeTest", {
+  extend : qx.core.Object,
+
+  members : {
+
+    testCreateDateTimeDate : function() {
+      var dateTime = this._createDefaultDateTimeTime();
+      assertTrue( dateTime instanceof org.eclipse.swt.widgets.DateTimeTime );
+      assertEquals( "datetime-time", dateTime.getAppearance() );
+      dateTime.destroy();
+    },
+
+    testSendAllFieldsTogether : function() {
+      var testUtil = org.eclipse.rwt.test.fixture.TestUtil;
+      testUtil.prepareTimerUse();
+      var dateTime = this._createDefaultDateTimeTime();
+      dateTime.setHours( 4 );
+      dateTime.setMinutes( 34 );
+      dateTime.setSeconds( 55 );
+      testUtil.clearRequestLog();
+      dateTime._sendChanges();
+      assertEquals( 0, testUtil.getRequestsSend() );
+      var req = org.eclipse.swt.Request.getInstance();
+      assertEquals( 4, req._parameters[ "w3.hours" ] );
+      assertEquals( 34, req._parameters[ "w3.minutes" ] );
+      assertEquals( 55, req._parameters[ "w3.seconds" ] );
+    },
+
+    testSendEvent : function() {
+      var testUtil = org.eclipse.rwt.test.fixture.TestUtil;
+      testUtil.prepareTimerUse();
+      var dateTime = this._createDefaultDateTimeTime();
+      dateTime.setHasSelectionListener( true );
+      dateTime.setHours( 4 );
+      dateTime.setMinutes( 34 );
+      dateTime.setSeconds( 55 );
+      testUtil.clearRequestLog();
+      dateTime._sendChanges(); 
+      // this should restart the timer, though there is currently no way to test it:
+      dateTime._sendChanges(); 
+      assertEquals( 0, testUtil.getRequestsSend() );
+      testUtil.forceInterval( dateTime._requestTimer );
+      assertFalse( dateTime._requestTimer.getEnabled() );
+      assertEquals( 1, testUtil.getRequestsSend() );
+      dateTime.destroy();
+
+    },
+
+    // see bug 358531
+    testEditingWithKeyboard_InitialEditing : function() {
+      var testUtil = org.eclipse.rwt.test.fixture.TestUtil;
+      var dateTime = this._createDefaultDateTimeTime();
+      dateTime.setHours( 2 );
+      dateTime.setMinutes( 2 );
+      dateTime.setSeconds( 2 );
+      testUtil.click( dateTime._minutesTextField );
+      testUtil.flush();
+      testUtil.pressOnce( dateTime, "1" );
+      testUtil.pressOnce( dateTime, "3" );
+      assertEquals( "13", dateTime._minutesTextField.getText() );
+      dateTime.destroy();
+    },
+
+    testEditingWithKeyboard_ContinualEditing : function() {
+      var testUtil = org.eclipse.rwt.test.fixture.TestUtil;
+      var dateTime = this._createDefaultDateTimeTime();
+      dateTime.setHours( 2 );
+      dateTime.setMinutes( 2 );
+      dateTime.setSeconds( 2 );
+      testUtil.click( dateTime._minutesTextField );
+      testUtil.flush();
+      testUtil.pressOnce( dateTime, "2" );
+      testUtil.pressOnce( dateTime, "1" );
+      testUtil.pressOnce( dateTime, "3" );
+      assertEquals( "03", dateTime._minutesTextField.getText() );
+      dateTime.destroy();
+    },
+
+    //////////
+    // Helpers
+
+    _createDefaultDateTimeTime : function() {
+      var testUtil = org.eclipse.rwt.test.fixture.TestUtil;
+      var style = "medium";
+      var dateTime = new org.eclipse.swt.widgets.DateTimeTime( style );
+      var widgetManager = org.eclipse.swt.WidgetManager.getInstance();
+      widgetManager.add( dateTime, "w3", true );
+      dateTime.setSpace( 3, 115, 3, 20 );
+      dateTime.addToDocument();
+      testUtil.flush();
+      return dateTime;
+    }
+
+  }
+
+} );
