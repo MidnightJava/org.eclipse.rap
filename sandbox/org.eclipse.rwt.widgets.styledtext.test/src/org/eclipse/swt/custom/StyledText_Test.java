1668bdbdba8a170e8df13a9e23544eba1b72cab4
rherrmann <rherrmann>
2011-10-06 08:11:05 +0000

    Removed duplicate CloneableCompatibility interface

--- sandbox/org.eclipse.rwt.widgets.styledtext.test/src/org/eclipse/swt/custom/StyledText_Test.java
+++ sandbox/org.eclipse.rwt.widgets.styledtext.test/src/org/eclipse/swt/custom/StyledText_Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2009, 2011 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.swt.custom;
 
@@ -20,13 +21,12 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class StyledText_Test extends TestCase {
+  private static final String TEXT = "Eclipse is an open source community.";
 
-  private static final String TXT = "Eclipse is an open source community.";
+  private StyledText styledText;
+
 
   public void testInitialValues() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
     assertEquals( "", styledText.getText() );
     assertEquals( 0, styledText.getCaretOffset() );
     assertEquals( new Point( 0, 0 ), styledText.getSelection() );
@@ -34,34 +34,31 @@ public class StyledText_Test extends TestCase {
   }
 
   public void testText() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
-    styledText.setText( TXT );
-    assertEquals( TXT, styledText.getText() );
-    assertEquals( TXT.length(), styledText.getCharCount() );
+    styledText.setText( TEXT );
+    assertEquals( TEXT, styledText.getText() );
+  }
+  
+  public void testSetTextWithNullArgument() {
     try {
       styledText.setText( null );
       fail( "Must not allow to set null-text." );
-    } catch( IllegalArgumentException e ) {
-      // expected
+    } catch( IllegalArgumentException expected ) {
     }
+  }
+  
+  public void testGetCharCount() {
+    styledText.setText( TEXT );
+    assertEquals( TEXT.length(), styledText.getCharCount() );
   }
 
   public void testSelectionText() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
-    styledText.setText( TXT );
+    styledText.setText( TEXT );
     styledText.setSelection( 14, 25 );
     assertEquals( "open source", styledText.getSelectionText() );
   }
 
   public void testSelection() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
-    styledText.setText( TXT );
+    styledText.setText( TEXT );
     styledText.setSelection( 0, 7 );
     assertEquals( new Point( 0, 7 ), styledText.getSelection() );
     styledText.setSelection( 10, 300 );
@@ -69,66 +66,61 @@ public class StyledText_Test extends TestCase {
   }
 
   public void testCaretOffset() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
-    styledText.setText( TXT );
+    styledText.setText( TEXT );
     styledText.setCaretOffset( 10 );
     assertEquals( 10, styledText.getCaretOffset() );
     styledText.setCaretOffset( 300 );
     assertEquals( 36, styledText.getCaretOffset() );
   }
+  
+  public void testGetStyleRange() {
+    styledText.setText( TEXT );
+    assertEquals( 0, styledText.getStyleRanges().length );
+  }
 
   public void testStyleRange() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
-    styledText.setText( TXT );
-    assertEquals( 0, styledText.getStyleRanges().length );
+    styledText.setText( TEXT );
+    
     StyleRange sr = new StyleRange();
     sr.start = 10;
     sr.length = 10;
     sr.fontStyle = SWT.BOLD;
     styledText.setStyleRange( sr );
     assertEquals( 1, styledText.getStyleRanges().length );
+    
     StyleRange sr1 = new StyleRange();
     sr1.start = 12;
     sr1.length = 6;
     sr1.fontStyle = SWT.ITALIC;
     styledText.setStyleRange( sr1 );
     assertEquals( 3, styledText.getStyleRanges().length );
-    StyleRange sr2 = new StyleRange();
-    sr2.start = 12;
-    sr2.length = 100;
-    sr2.fontStyle = SWT.ITALIC;
+  }
+  
+  public void testSetStyleRangeWithOutOfBoundsValue() {
+    StyleRange styleRange = new StyleRange();
+    styleRange.start = 12;
+    styleRange.length = 100;
+    styleRange.fontStyle = SWT.ITALIC;
     try {
-      styledText.setStyleRange( sr2 );
+      styledText.setStyleRange( styleRange );
       fail( "Must not allow to set range out of text bounds." );
-    } catch( IllegalArgumentException e ) {
-      // expected
+    } catch( IllegalArgumentException expected ) {
     }
   }
 
   public void testAdapter() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
-    styledText.setText( TXT );
-    IStyledTextAdapter adapter
-      = ( IStyledTextAdapter )styledText.getAdapter( IStyledTextAdapter.class );
+    styledText.setText( TEXT );
+    IStyledTextAdapter adapter = getStypedTextAdapter( styledText );
     String html = adapter.getHtml();
-    String expected = "<span id=sr0></span><span id=sel></span>"
-     + "<span id=sr0>Eclipse&nbsp;is&nbsp;an&nbsp;open&nbsp;source&nbsp;community.</span>";
+    String expected
+      = "<span id=sr0></span><span id=sel></span>"
+      + "<span id=sr0>Eclipse&nbsp;is&nbsp;an&nbsp;open&nbsp;source&nbsp;community.</span>";
     assertEquals( expected, html );
   }
 
   public void testGenerateHtml() {
-    Display display = new Display();
-    Shell shell = new Shell( display, SWT.SHELL_TRIM );
-    StyledText styledText = new StyledText( shell, SWT.NONE );
-    styledText.setText( TXT );
-    IStyledTextAdapter adapter
-      = ( IStyledTextAdapter )styledText.getAdapter( IStyledTextAdapter.class );
+    styledText.setText( TEXT );
+    IStyledTextAdapter adapter = getStypedTextAdapter( styledText );
     StyleRange sr = new StyleRange();
     sr.start = 10;
     sr.length = 10;
@@ -151,9 +143,16 @@ public class StyledText_Test extends TestCase {
 
   protected void setUp() throws Exception {
     Fixture.setUp();
+    Display display = new Display();
+    Shell shell = new Shell( display );
+    styledText = new StyledText( shell, SWT.NONE );
   }
 
   protected void tearDown() throws Exception {
     Fixture.tearDown();
+  }
+
+  private static IStyledTextAdapter getStypedTextAdapter( StyledText styledText ) {
+    return ( IStyledTextAdapter )styledText.getAdapter( IStyledTextAdapter.class );
   }
 }
